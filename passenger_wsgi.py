653d10bdaec21fb1a161d5f1e0a96916e310a3a7
chahineKeb <chahine.kebbab.formasup82@gmail.com>
2026-04-15 19:35:50 +0200

    Delete passenger_wsgi.py

--- passenger_wsgi.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-# Indique le dossier de l'application
-sys.path.insert(0, os.path.dirname(__file__))
-
-# "app" = nom du fichier app.py, "app" = l'objet Flask dedans
-from app import app as application
